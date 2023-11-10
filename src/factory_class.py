e1f8cc40f27327e4a1a18974624f7bca631fef27
SangDoo Nam <kexe009@hotmail.com>
2023-11-10 21:58:55 +0100

    sangdoo_nam

--- src/factory_class.py
+++ src/factory_class.py
@@ -1,10 +1,15 @@
 from worker import DeliveryMan, Packager
 
 
+
 # Complete the method create_object
 class WorkerFactory:
     @staticmethod
     def create_object(worker_type):
-        pass
+        if worker_type == 'delivery':
+            return DeliveryMan()
+        elif worker_type == 'packager':
+            return Packager()
+        raise TypeError('Unknown type')
 
 
--- src/warehouse.py
+++ src/warehouse.py
@@ -2,7 +2,25 @@ from time import time as tt, ctime as ct
 
 
 # Make this class a singleton
+
+def singlestone(cls):
+    instance = {}
+    def wrapped(*args, **kwargs):
+        if cls not in instance:
+            instance[cls] = cls(*args, **kwargs)
+        return instance[cls]
+    return wrapped
+
+@singlestone
 class Warehouse:
+    
+    # instance = None
+    
+    # def __new__(cls):
+    #     if cls.instance is None:
+    #         cls.instance = super().__new__(cls)
+    #     return cls.instance
+    
     def __init__(self):
         self.num_employees = 0
         self.creation_time = ct(tt())
